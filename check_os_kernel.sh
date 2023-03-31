208b4f9cd2bf1a4aaf3dd4e45e8f7a631f7a2e50
Rahul <rahul.unnikrishnan.nair@intel.com>
2023-03-31 11:05:57 -0700

    Update check_os_kernel.sh

--- check_os_kernel.sh
+++ check_os_kernel.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
-
+declare -A ubuntu_kernel_versions
+ubuntu_kernel_versions=(
+    ["20.04"]="5.14.0-1047-oem"
+    ["22.04"]="5.15.0-57-generic"
+)
 if command -v lsb_release &> /dev/null
 then
     os_version=$(lsb_release -d | awk '{print $2,$3,$4,$5}')
@@ -8,20 +11,18 @@ else
 fi
 
 kernel_version=$(uname -r)
-major_kernel_version=$(echo "$kernel_version" | cut -d. -f1,2)
-
 echo "OS Version: $os_version"
-echo "Kernel Version: $kernel_version"
-
-if [[ "$os_version" == *"Ubuntu"* && $(echo "$major_kernel_version >= 6.2" | bc) -eq 1 ]]; then
+major_kernel_version=$(echo "$kernel_version" | awk -F. '{print $1}')
+minor_kernel_version=$(echo "$kernel_version" | awk -F. '{print $2}')
+os_version_number=$(echo "$os_version" | grep -oP '\d{2}\.\d{2}')
+if [[ $major_kernel_version -gt 6 || ( $major_kernel_version -eq 6 && $minor_kernel_version -ge 2 ) ]] || [[ ${ubuntu_kernel_versions["$os_version_number"]} == "$kernel_version" ]]; then
+    echo "Kernel version is $kernel_version and has support for Intel dGPUs by default"
     exit 0
 else
-    if [[ "$os_version" == *"Ubuntu 22.04"* && "$kernel_version" != "5.15.0-57-generic" ]]; then
-        echo "Kernel version for Ubuntu 22.04 should be 5.15.0-57-generic"
-    elif [[ "$os_version" == *"Ubuntu 20.04"* && "$kernel_version" != "5.14.0-1047-oem" ]]; then
-        echo "Kernel version for Ubuntu 20.04 should be 5.14.0-1047-oem"
+    if [[ -n ${ubuntu_kernel_versions["$os_version_number"]} ]]; then
+        echo "Kernel version for Ubuntu $os_version_number should be ${ubuntu_kernel_versions["$os_version_number"]}"
     else
-        echo "Kernel version for $os_version is not supported. More details can be found at https://dgpu-docs.intel.com/installation-guides/ubuntu/ubuntu-focal-arc.html"
+        echo "Kernel version for $os_version is not supported. More details on how to setup dGPU drivers can be found at https://dgpu-docs.intel.com/installation-guides/ubuntu/ubuntu-focal-arc.html"
     fi
     exit 1
 fi
