1e4463d5feabbd947ba43d3130830d3fa74426d1
Rahul <rahul.unnikrishnan.nair@intel.com>
2023-03-31 11:10:54 -0700

    Update check_sycl.sh

--- check_sycl.sh
+++ check_sycl.sh
@@ -1,16 +1,32 @@
-#!/bin/env bash
-
 export CPLUS_INCLUDE_PATH=$(echo /usr/include/c++/*):$(echo /usr/include/x86_64-linux-gnu/c++/*):/usr/include
 export LIBRARY_PATH=$(dirname $(find /usr/lib /usr/lib64 -name "libstdc++.so" | head -n 1)):$LIBRARY_PATH
 
 if which icpx >/dev/null 2>&1; then
   echo "icpx is in the environment"
-cd ./sycl_samples/ && make all > /dev/null &&\
-  make run &&\
-  make clean > /dev/null
-echo "Test passed"
-echo "compile and execute sycl programs on the device"
-exit 0
+  cd ./sycl_samples/ && \
+  make all > /dev/null
+  build_status=$?
+  if [ $build_status -eq 0 ]; then
+    make run
+    run_status=$?
+    if [ $run_status -eq 0 ]; then
+      make clean > /dev/null
+      clean_status=$?
+      if [ $clean_status -eq 0 ]; then
+        echo "compile and execute sycl programs on the device"
+        exit 0
+      else
+        echo "Error: 'make clean' failed"
+        exit $clean_status
+      fi
+    else
+      echo "Error: 'make run' failed"
+      exit $run_status
+    fi
+  else
+    echo "Error: 'make all' failed"
+    exit $build_status
+  fi
 else
   echo "icpx is required to compile the program in sycl_samples, but it is not in the environment"
   exit 1
