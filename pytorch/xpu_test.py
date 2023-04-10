b5b5fec64088efac8e60fc13068766be7b9c7831
rahulunair <rahulunair@gmail.com>
2023-04-10 14:09:23 -0700

    adding further dtypes

--- pytorch/xpu_test.py
+++ pytorch/xpu_test.py
@@ -1,46 +1,63 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 import argparse
 import torch
 
-def test_random_multiplication():
-    x = torch.rand(1, 1).to('xpu', dtype=torch.float16)
-    y = torch.rand(1, 1).to('xpu', dtype=torch.float16)
-    z = x * y
-    print("Random FP16 multiplication:")
-    print("  Input x:", x.cpu())
-    print("  Input y:", y.cpu())
-    print("  Output z:", z.cpu())
+def test_random_multiplication(dtype=torch.float32):
+    try:
+        print(f"Random {dtype.__str__().split('.')} multiplication:")
+        x = torch.rand(1, 1).to('xpu', dtype=dtype)
+        y = torch.rand(1, 1).to('xpu', dtype=dtype)
+        z = x * y
+        print("  Input x:", x.cpu())
+        print("  Input y:", y.cpu())
+        print("  Output z:", z.cpu())
+    except Exception as e:
+        print(f"Error during {dtype.__str__().split('.')} random multiplication:", e)
+        exit(1)
 
-def test_specific_multiplication():
-    x = torch.tensor([[1.0, 2.0]]).to('xpu', dtype=torch.float16)
-    y = torch.tensor([[3.0, 4.0]]).to('xpu', dtype=torch.float16)
-    z_expected = torch.tensor([[3.0, 8.0]]).to('xpu', dtype=torch.float16)
-    z = x * y
-    print("Specific FP16 multiplication:")
-    print("  Input x:", x.cpu())
-    print("  Input y:", y.cpu())
-    print("  Output z:", z.cpu())
-    if torch.allclose(z, z_expected):
-        print("Calculation is correct")
-    else:
-        print("Calculation is incorrect")
+def test_specific_multiplication(dtype=torch.float32):
+    try:
+        print(f"Specific {dtype.__str__().split('.')} multiplication:")
+        x = torch.tensor([[1.0, 2.0]]).to('xpu', dtype=dtype)
+        y = torch.tensor([[3.0, 4.0]]).to('xpu', dtype=dtype)
+        z_expected = torch.tensor([[3.0, 8.0]]).to('xpu', dtype=dtype)
+        z = x * y
+        print("  Input x:", x.cpu())
+        print("  Input y:", y.cpu())
+        print("  Output z:", z.cpu())
+        if torch.allclose(z, z_expected):
+            print("Calculation is correct")
+        else:
+            print("Calculation is incorrect")
+    except Exception as e:
+        print(f"Error during {dtype.__str__().split('.')} specific multiplication:", e)
+        exit(1)
 
 def main(args):
     try:
-        # Set random seed for reproducibility
+        print(f"torch version: {torch.__version__}")
         torch.manual_seed(args.seed)
         import intel_extension_for_pytorch as ipex
         ipex.xpu.seed_all()
         if ipex.xpu.is_available():
+            print(f"ipex version: {ipex.__version__}")
             print("Intel XPU device is available")
             device_name = ipex.xpu.get_device_name()
-            print("Device name:", device_name)
+            print(f"Device name: {device_name}")
             if not ipex.xpu.has_fp64_dtype():
                 print("Warning: Native FP64 type not supported on this platform")
-            # Test random multiplication
-            test_random_multiplication()
-            # Test specific multiplication
-            print("Check if multiplication with two tensors gives expected value")
-            test_specific_multiplication()
+
+            data_types = [torch.int8, torch.int16, torch.int32, torch.int64, 
+                          torch.float16, torch.float32, torch.bfloat16, torch.float64]
+            for dtype in data_types:
+                if dtype == torch.float64 and not ipex.xpu.has_fp64_dtype():
+                    print("Skipping FP64 tests, as the device doesn't support it.")
+                    continue
+                test_random_multiplication(dtype)
+                test_specific_multiplication(dtype)
+
         else:
             print("Warning: Intel XPU device is not available")
             raise Exception("Intel XPU device not detected")
