43b9751ac6682b9fc6493b9a2f24f063fa13f0f4
Rahul Nair <rahulunair@users.noreply.github.com>
2023-04-24 11:02:54 -0700

    Update check_render_group.sh

--- check_render_group.sh
+++ check_render_group.sh
@@ -1,18 +1,17 @@
 render_group="render"
+exclude_users="nogroup"
 
 if ! getent group $render_group > /dev/null; then
     echo "Render group not found on this system..."
     exit 1
 fi
 
-regular_users=$(getent passwd | awk -F: '$3 >= 1000 && $3 != 65534 {print $1}')
-
-if [ -z "$(getent passwd | awk -F: '$3 >= 1000 {print $1}' | xargs -n1 id -nG | grep -v render | sort -u)" ]; then
-    echo "All users with UUID > 1000 are in render group"
+regular_users=$(getent passwd | awk -F: -v exclude_uids="65534" '$3 >= 1000 && $3 != 65534 && !($1 in exclude_uids) {print $1}')
+if [ -z "$(echo "$regular_users" | xargs -n1 id -nG | grep -v render | sort -u)" ]; then
+    echo "All users with UID >= 1000 are in render group"
     exit 0
 else
     echo "Users not in render group:"
-    getent passwd | awk -F: '$3 >= 1000 {print $1}' | xargs -n1 id -nG | grep -v render | sort -u
+    echo "$regular_users" | xargs -n1 id -nG | grep -v render | sort -u
     exit 1
 fi
-
