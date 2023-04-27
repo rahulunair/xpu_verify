64ac85a2b70d8130bf36eb17645cecbc4e156030
Rahul Nair <rahulunair@users.noreply.github.com>
2023-04-27 13:54:51 -0700

    Update check_pytorch.sh

--- check_pytorch.sh
+++ check_pytorch.sh
@@ -1,30 +1,31 @@
 #!/bin/env bash
 
 CURRENT_DIR=$(pwd)
-IMAGE_TAG="xpu-flex"  # can be xpu-max also
+IMAGE_TAG="xpu-flex"  # could be gpu-max also
 IMAGE_NAME="intel/intel-extension-for-pytorch"
 
 if command -v docker &> /dev/null
 then
-    echo "PyTorch test"
-    echo "pulling PyTorch docker image, if not found locally..."
+    echo "check if PyTorch can see the GPU (XPU) device"
+    echo "==================================================="
+    echo "pulling PyTorch docker image..."
     if docker pull "$IMAGE_NAME:$IMAGE_TAG"; then
-        docker run --rm \
-        -v "$CURRENT_DIR":/workspace \
-        -v /dev/dri/by-path:/dev/dri/by-path \
-        --device /dev/dri \
-        --privileged \
-        "$IMAGE_NAME:$IMAGE_TAG" python ./workspace/pytorch/xpu_test.py > output.txt
-
-        if grep -q "XPU tests successful!" output.txt; then
-            echo "Tests passed"
-            rm output.txt
-            exit 0
-        else
-            echo "Tests failed"
-            rm output.txt
-            exit 1
-        fi
+        CONTAINER_ID=$(docker run -d --rm --privileged $IMAGE_NAME:$IMAGE_TAG sleep infinity)
+        
+        docker cp ./pytorch/xpu_test.py $CONTAINER_ID:/xpu_test.py
+        docker exec $CONTAINER_ID python /xpu_test.py #> output.txt
+        
+        #if grep -q "XPU devices detected:" output.txt; then
+        #    echo "Tests passed"
+        #    rm output.txt
+        #    docker stop $CONTAINER_ID
+        #    exit 0
+        #else
+        #    echo "Tests failed"
+        #    rm output.txt
+        #    docker stop $CONTAINER_ID
+        #    exit 1
+        #fi
     else
         echo "Failed to pull PyTorch docker image."
         exit 1
